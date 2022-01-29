09969651f804161bf98ee8accd677bfa350ae739
Pavel Efimenko <work.efimenko@gmail.com>
2022-01-29 05:38:41 +0100

    Customize theme, add page background

---
+++ src/theme/components/heading/heading.js
@@ -0,0 +1,33 @@
+export const Heading = {
+  // style object for base or default style
+  baseStyle: {
+    fontWeight: 600,
+  },
+  // styles for different sizes ("sm", "md", "lg")
+  sizes: {
+    lg: {
+      fontSize: 20,
+    },
+  },
+  // styles for different visual variants ("outline", "solid")
+  variants: {
+    h1: {
+      fontSize: 48,
+    },
+    h2: {
+      fontSize: 36,
+    },
+    h3: {
+      fontSize: 32,
+    },
+    h4: {
+      fontSize: 26,
+    },
+    h5: {
+      fontSize: 20,
+    },
+    h6: {
+      fontSize: 16,
+    },
+  },
+};
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,12 +2,25 @@ import { extendTheme } from '@chakra-ui/react';
 import * as components from './components';
 
 export const theme = extendTheme({
+  fonts: {
+    body: 'Space Grotesk, serif',
+    heading: 'Sharp Grotesk, serif',
+  },
+  // fontSizes: {
+  //   lg: 20,
+  //   xl: '26px',
+  // },
   styles: {
     global: {
       // styles for the `body`
       body: {
-        fontFamily: 'Sharp Grotesk',
-        fontWeight: 600,
+        color: '#000',
+        height: '100%',
+        backgroundImage: 'url("/assets/images/merged-main-bg.jpg")',
+        backgroundPosition: 'fixed',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed, scroll',
       },
     },
   },
@@ -16,6 +29,16 @@ export const theme = extendTheme({
       bg: 'white',
       boxShadow: '0px 10px 35px rgba(136 120 172 / 15%)',
       borderRadius: 12,
+    },
+    bordered: {
+      bg: 'white',
+      borderRadius: 12,
+      border: '1px solid rgba(0 0 0 / 10%)',
+      transition: 'box-shadow 0.2s ease-in-out',
+
+      _hover: {
+        boxShadow: '0px 0px 30px rgba(0 0 0 / 20%)',
+      },
     },
   },
   components: {
