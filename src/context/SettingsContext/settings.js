6a1d0d0cc6fed58b6e6288d01c3217879bbe7de3
Pavel Efimenko <work.efimenko@gmail.com>
2022-02-17 01:43:57 +0100

    Add order field to default settings

--- src/context/SettingsContext/settings.js
+++ src/context/SettingsContext/settings.js
@@ -3,23 +3,27 @@ export const settings = {
     {
       id: 'statistics',
       visible: false,
+      order: 1,
     },
     {
       id: 'curated-collections',
       visible: true,
+      order: 2,
     },
     {
       id: 'top-collections',
       visible: false,
+      order: 3,
     },
     {
       id: 'user-collections',
       visible: false,
+      order: 4,
     },
     {
       id: 'my-watchlist',
       visible: false,
+      order: 5,
     },
   ],
-  //currency: ''
 };
