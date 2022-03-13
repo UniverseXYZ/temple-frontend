6eb9190efedcb4c01d23cef2d3bd9ef03503961a
Pavel Efimenko <work.efimenko@gmail.com>
2022-03-13 16:56:12 +0100

    Fix build

--- src/components/ui/ConfirmDialog/ConfirmDialog.tsx
+++ src/components/ui/ConfirmDialog/ConfirmDialog.tsx
@@ -11,7 +11,7 @@ import {
   //useDisclosure,
 } from '@chakra-ui/react';
 
-export const ConfirmDialog: FC = (props: any) => {
+export const ConfirmDialog: FC<any> = (props: any) => {
   //
   const {
     visible,
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,23 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { theme } from '@/theme/theme';
 
 export default class Document extends NextDocument {
+  //
+
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* <meta
+            httpEquiv="Content-Security-Policy"
+            content={`script-src 'nonce-2726c7f26c'`}
+          /> */}
+        </Head>
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <ColorModeScript
+            //@ts-ignore
+            //nonce={}
+            initialColorMode={theme.config.initialColorMode}
+          />
           <Main />
           <NextScript />
         </body>
