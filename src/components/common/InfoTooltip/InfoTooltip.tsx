2ae1b885bd6910d823954c3544d0b6b378a2c2b6
Pavel Efimenko <work.efimenko@gmail.com>
2022-03-25 05:05:00 +0100

    Feature: Add dark styles to InfoTooltip

--- src/components/common/InfoTooltip/InfoTooltip.tsx
+++ src/components/common/InfoTooltip/InfoTooltip.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from 'react';
-import { TooltipProps } from '@chakra-ui/react';
+import { TooltipProps, useColorMode } from '@chakra-ui/react';
 import { Tooltip } from '@/components/ui';
 import { InfoIcon } from '@/components/icons';
 
+import cn from 'classnames';
 import styles from './InfoTooltip.module.sass';
 
 type Props = Omit<TooltipProps, 'children'>;
 
 export const InfoTooltip: FC<Props> = (props) => {
   //
-  //const { label } = props;
+  const { colorMode } = useColorMode();
+
+  const isDark = colorMode === 'dark';
+
   return (
     <Tooltip openDelay={100} placement="top" {...props}>
-      <InfoIcon className={styles.InfoTooltip} cursor="pointer" />
+      <InfoIcon
+        className={cn(styles.InfoTooltip, isDark && styles.Dark)}
+        cursor="pointer"
+      />
     </Tooltip>
   );
 };
