6a0bcf9487e76d5c5d0aefa9542890b1c8a1ffa4
Pavel Efimenko <work.efimenko@gmail.com>
2022-05-09 08:49:50 +0200

    Add react-query provider and extend dayjs plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,31 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { SettingsProvider } from '@/context';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import type { AppProps } from 'next/app';
 import { theme } from '@/theme/theme';
 import '@/sass/style.sass';
 import 'swiper/css';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+const queryClient = new QueryClient();
+
 function App({ Component, pageProps }: AppProps) {
+  //
   return (
-    <SettingsProvider>
-      <ChakraProvider theme={theme}>
-        <div suppressHydrationWarning>
-          {typeof window === 'undefined' ? null : <Component {...pageProps} />}
-        </div>
-      </ChakraProvider>
-    </SettingsProvider>
+    <QueryClientProvider client={queryClient}>
+      <SettingsProvider>
+        <ChakraProvider theme={theme}>
+          <div suppressHydrationWarning>
+            {typeof window === 'undefined' ? null : (
+              <Component {...pageProps} />
+            )}
+          </div>
+        </ChakraProvider>
+      </SettingsProvider>
+    </QueryClientProvider>
   );
 }
 
