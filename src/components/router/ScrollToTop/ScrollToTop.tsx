38b6d4349c64ee356d2fd96a08377d51a2840019
Pavel Efimenko <work.efimenko@gmail.com>
2022-05-20 01:35:56 +0200

    ScrollToTop router component

---
+++ src/components/router/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router';
+
+export const ScrollToTop = (props: any) => {
+  //
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{props.children}</>;
+};
