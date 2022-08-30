94c4b39fc725534374d7c2b49301753bddfe63b1
rshtirmer <rshtirmer@gmail.com>
2022-08-30 10:58:55 -0400

    only show owned nfts if count > 0

--- src/components/pages/Collection/tabs.js
+++ src/components/pages/Collection/tabs.js
@@ -1,21 +1,32 @@
 import { OwnedNfts, Description, Activity } from './components/tabs';
 
 export const tabs = (tokenCount, userNFTs, description) => {
-  return [
-  {
-    id: 1,
-    title: 'Owned NFTs',
-    component: <OwnedNfts userNFTs={userNFTs}/>,
-    ticker: `${tokenCount}`,
-  },
-  {
-    id: 2,
-    title: 'Description',
-    component: <Description text={description}/>,
-  },
-  {
-    id: 3,
-    title: 'Collection activity',
-    component: <Activity />,
-  },
-]};
+  const tabsToReturn = [];
+  if(tokenCount > 0){
+    tabsToReturn.push(
+      {
+        id: 1,
+        title: 'Owned NFTs',
+        component: <OwnedNfts userNFTs={userNFTs}/>,
+        ticker: `${tokenCount}`,
+      }
+    )
+  }
+  if(description) {
+    tabsToReturn.push(
+      {
+        id: 2,
+        title: 'Description',
+        component: <Description text={description}/>,
+      }
+    )
+  }
+  tabsToReturn.push(
+    {
+      id: 3,
+      title: 'Collection activity',
+      component: <Activity />,
+    }  
+  )
+  return tabsToReturn;
+};
